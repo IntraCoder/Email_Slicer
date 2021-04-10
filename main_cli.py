d4a92629d44c79ded6cff618a5136e66d44c93cb
IntraCoder <intracoder569@gmail.com>
2021-04-10 11:04:39 +0530

    Update main_cli.py

--- main_cli.py
+++ main_cli.py
@@ -1,11 +1,32 @@
-print("------------------------ URL Slicer ------------------------")
+def validity(domain, username):
+    """ Function to check if Email is valid or not."""
+    conditions = ("." in domain), (domain[0] != "."), (domain[-1] != "."), \
+                 (username != ''), ("." in username)
 
-url = input("\n>>> Enter URL : ").strip()
+    if all(conditions):
+        return True
+    else:
+        print("\033[1;31m[-] Invalid Email.")
+        return False
 
-if "http" in url:
-    security = url.split("/")[0]
-    slice_url = url.split("/")[2]
-else:
-    slice_url = url.split("/")[0]
 
-print("[+] Sliced URL :", slice_url)
+def slicer(mail):
+    """ Function to slice and return domain and username of the email. """
+    domain = mail.split("@")[-1]
+    ind = mail.find(domain)
+    username = mail[:ind - 1]
+    return domain, username
+
+
+if __name__ == '__main__':
+    print('\033[1;35m--------------------------- Email Slicer ---------------------------')
+    while 1:
+        email = input("\033[1;;m>>> Enter Email here (Quit to exit program): ").strip()
+
+        if "@" not in email:
+            print("\033[1;31m [-] Invalid Email.")
+        else:
+            domain, username = slicer(email)
+            if validity(domain, username):
+                print(f"\033[1;35m [+] Username :\033[1;34m {username}")
+                print(f"\033[1;35m [+] Domain   :\033[1;34m {domain}")
